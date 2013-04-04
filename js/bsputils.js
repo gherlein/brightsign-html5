d854526e5b10950377f4409ff572b980be772f0b
Greg Herlein <gherlein@herlein.com>
2013-04-04 15:55:23 -0700

    code cleanup, udp working,uservar working

--- js/bsputils.js
+++ js/bsputils.js
@@ -8,68 +8,54 @@ debug_log("screenWidth: "+screenWidth+" screenHeight "+screenHeight);
 
 var bsbrowser=isBrightSign();
 
-var bsp={ 
-    status:               "UNKNOWN",
-    unitName:             "none",
-    unitNamingMethod:     "none",
-    unitDescription:      "none",
-    serialNumber:         "none",
-    functionality:        "none"
+
+window.onload = function() {
+    getID(printObj);
+    getUserVars(printObj);
+    getUDPEvents(printObj);
 };
 
-var bspUserVars={
-    status:               "UNKNOWN"
+
+function bspID(unitName,unitNamingMethod,unitDescription,serialNumber,functionality)
+{
+    this.unitName=unitName;
+    this.unitNamingMethod=unitNamingMethod;
+    this.unitDescription=unitDescription;
+    this.serialNumber=serialNumber;
+    this.functionality=functionality;
 }
 
 
-window.onload = function() {
-    //getID();
-    getUserVars();
-};
-
-
-function getID()
+function getID(callback)
 {
     if(bsbrowser=true)
     {
        $.get('http://localhost:8080/GetID',function(data,status,jqXHR)
        { 
-            //console.log(status); 
-            un=jqXHR.responseText;
-            //console.log(un);
+            $xml = $( $.parseXML( jqXHR.responseText ));
+            
+            var n= $unitName = $xml.find( "unitName" ).text();
+            var nM=$unitNamingMethod = $xml.find( "unitNamingMethod" ).text();
+            var nD=$unitDescription = $xml.find( "unitDescription" ).text();
+            var s =$serialNumber = $xml.find( "serialNumber" ).text();
+            var f =$functionality = $xml.find( "functionality" ).text();
 
-            xmlDoc = $.parseXML( un );
-            $xml = $( xmlDoc );
-            $unitName = $xml.find( "unitName" );
-           // console.log($unitName.text());
-
-            $unitNamingMethod = $xml.find( "unitNamingMethod" );
-            //console.log($unitNamingMethod.text());
-
-            $unitDescription = $xml.find( "unitDescription" );
-            //console.log($unitDescription.text());
-
-            $serialNumber = $xml.find( "serialNumber" );
-            //console.log($serialNumber.text());
-
-            $functionality = $xml.find( "functionality" );
-            //console.log($functionality.text());
-
-            bsp={ 
-                status:               "OK",
-                unitName:             $unitName.text(),
-                unitNamingMethod:     $unitNamingMethod.text(),
-                unitDescription:      $unitDescription.text(),
-                serialNumber:         $serialNumber.text(),
-                functionality:        $functionality.text()
-            };
-            debug_log(JSON.stringify(bsp));
+            var b=new bspID(n,nM,nD,s,f);
+            callback(b);
         });
     }
 }
 
-function getUserVars()
+
+function userVar(key,value)
 {
+    this.key=key;
+    this.value=value;
+}
+
+function getUserVars(callback)
+{
+    var varlist=new Array();
     if(bsbrowser=true)
     {
        $.get('http://localhost:8080/GetUserVars',function(data,status,jqXHR)
@@ -79,22 +65,72 @@ function getUserVars()
             xmlDoc = $.parseXML( un );
             $xml = $( xmlDoc );
 
-            bspUserVars={ 
-                status:               "OK",
-            };
-
             $xml.find('BrightSignUserVariables').each(function(){
                 $(this).children().each(function(){
                     var tagName=this.tagName;
                     var val=$(this).text();
-                    bspUserVars[tagName]=val;
+                    uv=new userVar(tagName,val);
+                    varlist.push(uv);
                 })
             });
-            debug_log(JSON.stringify(bspUserVars));
+            callback(varlist);
         });
     }
 }
 
+
+function bspUDPEventList(sendPort,recvPort,evList)
+{
+    this.sendPort=sendPort;
+    this.recvPort=recvPort;
+    this.events=evList;
+}
+
+
+function udpevent(label,action)
+{
+    this.label=label;
+    this.action=action;
+}
+
+
+
+function getUDPEvents(callback)
+{
+    if(bsbrowser=true)
+    {
+       $.get('http://localhost:8080/GetUDPEvents',function(data,status,jqXHR)
+       { 
+            un=jqXHR.responseText;
+
+            xmlDoc = $.parseXML( un );
+            $xml = $( xmlDoc );
+
+            var recvPort=$xml.find( "receivePort").text();
+            var sendPort=$xml.find( "destinationPort" ).text();
+            var evlist= new Array();
+
+            $xml.find('udpEvents').each(function(){
+                $(this).children().each(function(){
+                    var label =$(this).find("label").text();
+                    var action=$(this).find("action").text();
+                    var ev=new udpevent(label,action);
+                    evlist.push(ev);
+                });
+            });
+
+            b=new bspUDPEventList(sendPort,recvPort,evlist);
+            callback(b);
+        });
+
+    }
+}
+
+
+function printObj(obj)
+{
+    debug_log(JSON.stringify(obj));
+}
 
 
 function isBrightSign()
